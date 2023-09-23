b5133d4b5b93328753b4cfc375aa3d401b57f4fa
1NAYRU <desent.antuan2017@gmail.com>
2023-09-23 09:17:59 +0500

    Update q.php

--- PF5/q.php
+++ PF5/q.php
@@ -1,11 +1,11 @@
 <?php
-    $host       = "b13.19oi.local";      // Адрес сервера базы данных
-    $dbname     = "b13_typical";    // Имя базы данных
-    $user       = "root";           // Имя пользователя
-    $password   = "1";               // Пароль
+    $host       = "db4.myarena.ru";      // Адрес сервера базы данных
+    $dbname     = "u19978_b13";    // Имя базы данных
+    $user       = "u19978_b13";           // Имя пользователя
+    $password   = "dP2fU4fM8u";               // Пароль
     $connection = new PDO('mysql:host=b13.19oi.local;dbname=b13_typical;charset=utf8', 'root', 'password');
 
-    $result = $connection->query('SELECT * FROM `TABLE_NAME`');
+    $result = $connection->query('SELECT * FROM `product`');
     while($row = $result->fetch( PDO::FETCH_ASSOC )){
     foreach ($row as $key => $value) {
         echo "<td>".$value."</td>";
