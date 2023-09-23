25df619d92f6934bbc7ae4f5b97b1333c8287dba
1NAYRU <desent.antuan2017@gmail.com>
2023-09-23 09:45:11 +0500

    Update q.php

--- PF5/q.php
+++ PF5/q.php
@@ -3,12 +3,18 @@
     $dbname     = "u19978_b13";    // Имя базы данных
     $user       = "u19978_b13";           // Имя пользователя
     $password   = "dP2fU4fM8u";               // Пароль
-    $connection = new PDO('mysql:host=b13.19oi.local;dbname=b13_typical;charset=utf8', 'root', 'password');
+    $connection = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
 
+    echo '<table tellspacing="1" bgcolor="#000000">';
     $result = $connection->query('SELECT * FROM `product`');
+
     while($row = $result->fetch( PDO::FETCH_ASSOC )){
-    foreach ($row as $key => $value) {
-        echo "<td>".$value."</td>";
+        echo'<tr bgcolor="#ffffff">';
+        foreach ($row as $key => $value) {
+            echo "<td>".$value."</td>"; 
+        }
+        echo'</tr>';
     }
-}
+    echo'</table>';
+
 ?>
