c00265eefc9e5885754b8281e94f1e942bb1eab4
1NAYRU <desent.antuan2017@gmail.com>
2023-09-23 10:36:26 +0500

    Update q.php

--- PF5/q.php
+++ PF5/q.php
@@ -5,16 +5,33 @@
     $password   = "dP2fU4fM8u";               // Пароль
     $connection = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
 
-    echo '<table tellspacing="1" bgcolor="#000000">';
     $result = $connection->query('SELECT * FROM `product`');
 
-    while($row = $result->fetch( PDO::FETCH_ASSOC )){
-        echo'<tr bgcolor="#ffffff">';
-        foreach ($row as $key => $value) {
+    If ($result){
+    if ($result->rowCount() == 0) {
+        echo "таблица пустая";
+    } else {
+    
+    echo "<table border='1'>";
+
+    echo "<tr>";
+    for($i - 0; $i < $result->columnCount();$i++){
+        $column = $result->getColumnMeta($i);
+        echo "<th>{$column['name']}</th>";
+    }
+    echo "</tr>";
+    
+    while($row = $result->fetch( PDO::FETCH_ASSOC)){
+        echo'<tr>';
+        foreach ($row as $value) {
             echo "<td>".$value."</td>"; 
         }
         echo'</tr>';
     }
     echo'</table>';
+    } }
+        else { 
+            echo 'запрос выполнен с ошибкой';
+        }
 
 ?>
