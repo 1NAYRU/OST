6e0377c90dea907d7caaae3ab7e1fd94e08956ba
1NAYRU <desent.antuan2017@gmail.com>
2023-09-30 11:31:19 +0500

    Update q.php

--- PF6/q.php
+++ PF6/q.php
@@ -5,7 +5,7 @@
         if(empty($_POST['name'])) exit('Поле "имя" не заполнено');
         if(empty($_POST['age'])) exit('Поле "возраст" не заполнено');
         if((int)$_POST['age'] < 1) exit('Ваш возраст введено отрицательное или НЕ числовое значение');
-
+        
         echo 'Здравствуйте, '.htmlspecialchars($_POST['name']).'. <br>';
         echo 'Вам '.(int)$_POST['age'].' лет.'.'<br>';
     }
