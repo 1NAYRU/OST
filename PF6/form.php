4b418eecf8b5bde36b76b36902ec5ed71f39712f
1NAYRU <desent.antuan2017@gmail.com>
2023-09-30 09:51:03 +0500

    Create form.php

---
+++ PF6/form.php
@@ -0,0 +1,14 @@
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>ФБР</title>
+    </head>
+    <body>
+        <form action="q.php" method="post">
+            <p>Ваше имя: <input type="text" name="name"></p>
+            <p>Ваш возраст: <input type="text" name="age"></p>
+            <p><input type="submit"></p> 
+        </form>
+    </body>
+</html>
