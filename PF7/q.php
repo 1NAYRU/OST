86454776c569b251b1ff5c753b033351ad5d1fd1
1NAYRU <desent.antuan2017@gmail.com>
2023-10-13 10:27:50 +0500

    Update q.php

--- PF7/q.php
+++ PF7/q.php
@@ -5,7 +5,7 @@
  $password   = "dP2fU4fM8u";               // Пароль
  $connection = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
 
-        if( $_SERVER['REQUEST_METHOD'] !== 'POST' ) exit;{
+        if( $_SERVER['REQUEST_METHOD'] !== 'POST' ) exit;
         
                 if(empty($_POST['name'])) exit('Поле "логин" не заполнено');
                 if(empty($_POST['password'])) exit('Поле "пароль" не заполнено');
@@ -14,14 +14,17 @@
                 if($len<8)exit('Минимальное длина пароля 8 символов');
                 if($_POST['password'] !== $_POST['password1']) exit('Пороли не совподают');
                 if(empty($_POST['mail'])) exit('Поле "почта" не заполнено');
-        }
+        
+        $pas_hash=password_hash($_POST['password'],PASSWORD_DEFAULT);
+        
         //создание нового пользователя
-        $date = [ $_POST['name'], $_POST['password'], $_POST['Email']];
+        $date = [ $_POST['name'], $pas_hash, $_POST['mail']];
         $res = $connection->prepare("INSERT INTO `PF7_users` (`name`,`password`,`Email`) VALUES (?,?,?);");
         $res = $res->execute($date);
         if($res){
                 exit('Регистрация прошла успешно');
         }
         exit('Ошибка регистрации');
+        
 ?>
   
